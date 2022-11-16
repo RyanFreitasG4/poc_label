9d8eb95c0c7e7765d162cfb3da4c309546852605
RyanFreitasG4 <ryan@g4tech.com.br>
2022-11-16 14:55:02 -0300

    add mixin para impressao de imagem

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/services.dart';
 import 'package:esc_pos_printer/esc_pos_printer.dart';
 import 'package:esc_pos_utils/esc_pos_utils.dart';
-import 'package:flutter/material.dart' hide Image;
+import 'package:flutter/material.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
-import 'package:image/image.dart';
-import 'package:intl/intl.dart';
 import 'package:poc_label/components/custom_dialog_mixin.dart';
 import 'package:poc_label/components/menu_widget.dart';
+import 'package:poc_label/mixins/print_mixin.dart';
 import 'package:poc_label/pages/settings_page.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -17,7 +16,8 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> with CustomDialogMixin {
+class _HomePageState extends State<HomePage>
+    with CustomDialogMixin, PrintReceipt {
   late TextEditingController _docController;
   late String ipPrinter;
   late String portPrinter;
@@ -117,49 +117,16 @@ class _HomePageState extends State<HomePage> with CustomDialogMixin {
         content: 'Conexão com a impressora efetuada com sucesso!',
         actions: [
           TextButton(
-            onPressed: () => () => Navigator.of(context).pop(),
+            onPressed: () => Navigator.of(context).pop(),
             child: const Text('Fechar'),
           )
         ],
       );
-      printDemoReceipt(printer);
+      await printDemoReceipt(printer);
       printer.disconnect();
     } else {
       print(res);
     }
-  }
-
-  Future<void> printDemoReceipt(NetworkPrinter printer) async {
-    // Print image
-    // final ByteData data = await rootBundle.load('assets/logo.jpg');
-    // final Uint8List bytes = data.buffer.asUint8List();
-    // final Image image = decodeImage(bytes)!;
-    // printer.imageRaster(image);
-
-    // final List<int> barData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 4];
-    // printer.barcode(Barcode.upcA(barData));
-
-    printer.text('------------------------------',
-        styles: PosStyles(align: PosAlign.center), linesAfter: 1);
-    printer.text('=== G4TECH ===', styles: PosStyles(align: PosAlign.center));
-    printer.text('ENDEREÇO::', styles: PosStyles(align: PosAlign.center));
-    printer.text('Rua dos Andradas, 220',
-        styles: PosStyles(align: PosAlign.center));
-    printer.text('Centro, Cornélio Procópio - PR',
-        styles: PosStyles(align: PosAlign.center));
-    printer.text('ENTRE EM CONTATO:',
-        styles: PosStyles(align: PosAlign.center));
-    printer.text('Tel: (43) 99956-2946',
-        styles: PosStyles(align: PosAlign.center));
-    printer.text('Email: contato@g4tech.com.br',
-        styles: PosStyles(align: PosAlign.center));
-    printer.text('Web: https://www.g4tech.com.br/',
-        styles: PosStyles(align: PosAlign.center), linesAfter: 1);
-    printer.text('--------------------------------',
-        styles: PosStyles(align: PosAlign.center));
-
-    printer.feed(2);
-    printer.cut();
   }
 
   @override
